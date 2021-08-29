616b03b53e2cb57bf3ffd0d8a4729c2de4688d23
Sanket Debnath <sanketdebnath24@gmail.com>
2021-08-29 15:34:57 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db');
 const transactions = require('./routes/transactions');
+
 const PORT = process.env.PORT || 8000;
+
 const app = express();
 
 connectDB();
 
 app.use(express.json());
-
+app.use(cors({ origin: true, credentials: true }));
 app.get('/', (req, res) => {
   res.send('Server is up');
 });
