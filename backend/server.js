e30507e9a37a679997204e53a2183780db05a3b9
Sanket Debnath <sanketdebnath24@gmail.com>
2021-08-29 14:12:38 +0530

    defining api route

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
-
+const transactions = require('./routes/transactions');
 const PORT = process.env.PORT || 8000;
 const app = express();
 
@@ -9,4 +9,9 @@ connectDB();
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Server is up');
+});
+
+app.use('/api/transactions', transactions);
 app.listen(PORT, console.log(`SERVER running on ${PORT}`));
